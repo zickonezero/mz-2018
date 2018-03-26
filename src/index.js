313da23616e5975ff542f6efa4864b3875f464d2
Michael Zick <michaelzick@gmail.com>
2018-03-25 22:23:49 -0700

    MISC: removed breaking file/module refs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App />
 );
 
 ReactDOM.render(app, document.getElementById('root'));
