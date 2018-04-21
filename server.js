a58feaa780393b7ee6425de3b3e01aff045314f3
Michael Zick <michaelzick@gmail.com>
2018-04-21 16:37:22 -0700

    PACKAGES: removed express; changed start to dev

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.render('index.html'))
-
-// app.use(express.static(__dirname + 'src/index.html'))
-
-app.listen(8081, () => console.log('Example app listening on port 8081!'))
