31f42917322ac76a188d293f7c60e7b557d3238d
Michael Zick <michaelzick@gmail.com>
2018-04-21 16:31:56 -0700

    WEBPACK: got prod server config working

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -24,32 +24,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
-                        }
-                     },
-                     {
-                         loader: 'postcss-loader',
-                         options: {
-                             ident: 'postcss',
-                             plugins: () => [
-                                 autoprefixer({
-                                     browsers: [
-                                        "> 1%",
-                                        "last 2 versions"
-                                     ]
-                                 })
-                             ]
-                         }
-                      }
-                ]
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
